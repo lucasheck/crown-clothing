78314db9ccc0eccbe7dfea54eaee341dbcebe114
lucasheck <lucasheck23@gmail.com>
2022-11-02 23:57:46 -0400

    1 - Adding routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home";
 import Navigation from "./routes/navigation/navigation";
+import SignIn from "./routes/sign-in/sign-in";
+
+const Shop = () => {
+	return <h1>Shop Section</h1>;
+};
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Navigation />}>
 				<Route index element={<Home />} />
+				<Route path="shop" element={<Shop />} />
+				<Route path="signin" element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -1,11 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+
+import "./navigation.scss";
 
 const Navigation = () => {
 	return (
-		<div>
-			<h1>I am the navigation!</h1>
+		<Fragment>
+			<div className="navigation">
+				<Link className="logo-container" to="/">
+					<CrownLogo className="logo" />
+				</Link>
+				<div className="nav-links-container">
+					<Link className="nav-link" to="/shop">
+						SHOP
+					</Link>
+					<Link className="nav-link" to="/contact">
+						CONTACT
+					</Link>
+					<Link className="nav-link" to="/signin">
+						SIGN IN
+					</Link>
+				</div>
+			</div>
 			<Outlet />
-		</div>
+		</Fragment>
 	);
 };
 
